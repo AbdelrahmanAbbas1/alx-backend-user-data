c9354c32f4edaff25351fe853a6095d40272ab39
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-12-15 10:32:41 +0200

    Adding new file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """"This module obfuscate the log message"""
 import re
+from typing import List, Dict
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """returns the log messa/>ge obfuscated"""
-    patterns = {key: f'{key}=[^{separator}]' for key in fields}
+    patterns: Dict[str: str] = {key: f'{key}=[^{separator}]' for key in fields}
     for field, pattern in patterns.items():
         message = re.sub(pattern, f'{field}={redaction}', message)
     return message
